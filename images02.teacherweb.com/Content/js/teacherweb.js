135695a0fdfc6a52b93dca7c58025d3b8a304796
TranNicholas <ntranunix@gmail.com>
2017-12-02 10:39:50 -0800

    Initial commit of Padilla's website archive

---
+++ images02.teacherweb.com/Content/js/teacherweb.js
@@ -0,0 +1,59 @@
+﻿(function() {
+    var _tmplCache = {}
+    this.parseTemplate = function(str, data) {
+        /// <summary>
+        /// Client side template parser that uses &lt;#= #&gt; and &lt;# code #&gt; expressions.
+        /// and # # code blocks for template expansion.
+        /// NOTE: chokes on single quotes in the document in some situations
+        ///       use &amp;rsquo; for literals in text and avoid any single quote
+        ///       attribute delimiters.
+        /// </summary>
+        /// <param name="str" type="string">The text of the template to expand</param>
+        /// <param name="data" type="var">
+        /// Any data that is to be merged. Pass an object and
+        /// that object's properties are visible as variables.
+        /// </param>
+        /// <returns type="string" />
+        var err = "";
+        try {
+            var func = _tmplCache[str];
+            if (!func) {
+                var strFunc =
+            "var p=[],print=function(){p.push.apply(p,arguments);};" +
+                        "with(obj){p.push('" +
+                        str.replace(/[\r\t\n]/g, " ")
+                           .replace(/'(?=[^#]*#>)/g, "\t")
+                           .split("'").join("\\'")
+                           .split("\t").join("'")
+                           .replace(/<#=(.+?)#>/g, "',$1,'")
+                           .split("<#").join("');")
+                           .split("#>").join("p.push('")
+                           + "');}return p.join('');";
+                func = new Function("obj", strFunc);
+                _tmplCache[str] = func;
+            }
+            return func(data);
+        } catch (e) { err = e.message; }
+        return "< # ERROR: " + err + " # >";
+    }
+})();
+
+/* For use with RadioButtons in Repeaters: http://www.codeguru.com/csharp/csharp/cs_controls/custom/article.php/c12371/ */
+function SetUniqueRadioButton(nameregex, currentId, hiddenFieldId) {
+/*
+        re = new RegExp(nameregex);
+        for (i = 0; i < document.forms[0].elements.length; i++) {
+            elm = document.forms[0].elements[i]
+            if (elm.type == 'radio') {
+                if (re.test(elm.name)) {
+                    elm.checked = false;
+                }
+            }
+        }
+*/
+        $('input:radio[name$="' + nameregex.split("*")[1] + '"]').attr('checked', false);
+
+        document.getElementById(currentId).checked = true;
+        var labelText = $('label[for="' + currentId + '"]');
+        $("#" + hiddenFieldId).val(labelText.html());
+}
---
+++ teacherweb.com/assets/js/googleanalytics.js
@@ -0,0 +1,24 @@
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-3419456-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+   
+var _elqQ = _elqQ || [];
+_elqQ.push(['elqSetSiteId', '2376']);
+_elqQ.push(['elqTrackPageView']);
+
+(function () {
+    function async_load() {
+        var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true;
+        s.src = 'http://img.en25.com/i/elqCfg.min.js';
+        var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+    }
+    if (window.addEventListener) window.addEventListener('DOMContentLoaded', async_load, false);
+    else if (window.attachEvent) window.attachEvent('onload', async_load); 
+})();
